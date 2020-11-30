197ae80e9e20800c88fdc2a91407b6e5059a9bd9
arjunseth <setharjun957@gmail.com>
2020-11-30 17:52:57 +0530

    minor changes- ARJUN

--- LR0Parser.py
+++ LR0Parser.py
@@ -1,37 +1,16 @@
 #---------------- Importing Necessary Libraries ----------------
-
-import os
-import time
-from collections import Counter
-import pyfiglet
 import termtables as tt
 
-#----------------Done Importing Necessary Libraries ----------------
+#----------------Done Importing Necessary Library ----------------
 
-title = pyfiglet.figlet_format("LR (0) Parsing", font="digital")
-print(title)
+
+print("------------LR(0) PARSING-------------")
 
 #----------------Defining Important Functions ----------------
 def addDot(dot):
     addDotVar = dot.replace("->", "->.")
     return addDotVar
 
-def compressedName(name: str):
-    compResult = Counter(name)
-    comp = ''
-    for r in compResult:
-        comp += r + str(compResult[r])
-    return comp
-
-#Function to Save file
-def saveFile(final_string, grammar, name):
-    directory = os.path.dirname("stringParsing/" + str(grammar) + "/")
-    if not os.path.exists(directory):
-        print("Creating this Director......")
-        os.makedirs(directory)
-
-    with open("stringParsing/{0}/{1}.txt".format(grammar, name), 'w') as fileParsing:
-        fileParsing.write(final_string)
 
 #Function to find closure
 def findClosure(gram):
