d3abae3b17566af008b6769c9957003af45d2778
Minhas-221 <65837223+Minhas-221@users.noreply.github.com>
2020-12-02 11:39:10 +0530

    Update LR0Parser.py

--- LR0Parser.py
+++ LR0Parser.py
@@ -94,7 +94,7 @@ def getList(graph, state):
 
 #---------------- Done Implementing Functions  ----------------
 
-
+#---------------- Augmented Grammar ----------------
 productionRules = []
 itemSet = []
 flag = []
